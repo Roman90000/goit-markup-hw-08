e6f502a58ac91d951a1b5735a680932b4a4a0390
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2022-08-10 20:29:43 +0300

    Осталось сделать backdrop

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menu: document.querySelector("[data-menu]"),
+    openMenuBtn: document.querySelector(".btn-mob-open"),
+    closeMenuBtn: document.querySelector(".btn-mobile-close"),
+    menu: document.querySelector(".mob-menu"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-  function toggleModal() {
+  function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
   }
 })();
